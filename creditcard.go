75cd9da0b3f43f4b6d58a913a55d80f1d59136a5
SilverOmega <nguyenducanh8993@gmail.com>
2023-08-10 15:36:35 +0700

    update Luhn Algorithm

--- creditcard.go
+++ creditcard.go
@@ -79,11 +79,11 @@ func (c *Card) validate(skipCvv bool, allowTestNumbers ...bool) error {
 	}
 	// Validate the expiration year
 	if year < time.Now().UTC().Year() {
-		return errors.New("Card has Expried")
+		return errors.New("Credit Card has Expried")
 	}
 	// Validate the expiration year and month
 	if year == time.Now().UTC().Year() && month < int(time.Now().UTC().Month()) {
-		return errors.New("Card has Expried")
+		return errors.New("Credit Card has Expried")
 	}
 
 	// Validate CVV langth
@@ -92,7 +92,7 @@ func (c *Card) validate(skipCvv bool, allowTestNumbers ...bool) error {
 	}
 	// Validate Card number length
 	if len(c.Number) < 13 {
-		return errors.New("Invalid Card Number")
+		return errors.New("Invalid Credit Card Number")
 	}
 
 	switch c.Number {
@@ -126,12 +126,98 @@ func (c *Card) Bard() error {
 
 // BrandValidate adds/checks/verifies the credit card's company / issuer
 func (c *Card) BrandValidate() (Company, error) {
-	// implemented here
-	return Company{}, nil
+	ccLen := len(c.Number)
+	ccDigits := digits{}
+	var err error
+
+	for i := 0; i < 6; i++ {
+		if i < ccLen {
+			if ccDigits[i], err = strconv.Atoi(c.Number[:i+1]); err != nil {
+				return Company{"", ""}, errors.New("Unknow Credit Card Brand")
+			}
+		}
+	}
+	switch {
+	case ccDigits.At(4) == 4011 || ccDigits.At(6) == 431274 || ccDigits.At(6) == 438935 ||
+		ccDigits.At(6) == 451416 || ccDigits.At(6) == 457393 || ccDigits.At(4) == 4576 ||
+		ccDigits.At(6) == 457631 || ccDigits.At(6) == 457632 || ccDigits.At(6) == 504175 ||
+		ccDigits.At(6) == 627780 || ccDigits.At(6) == 636297 || ccDigits.At(6) == 636368 ||
+		ccDigits.At(6) == 636369 || (ccDigits.At(6) >= 506699 && ccDigits.At(6) <= 506778) ||
+		(ccDigits.At(6) >= 509000 && ccDigits.At(6) <= 509999) ||
+		(ccDigits.At(6) >= 650031 && ccDigits.At(6) <= 650051) ||
+		(ccDigits.At(6) >= 650035 && ccDigits.At(6) <= 650033) ||
+		(ccDigits.At(6) >= 650405 && ccDigits.At(6) <= 650439) ||
+		(ccDigits.At(6) >= 650485 && ccDigits.At(6) <= 650538) ||
+		(ccDigits.At(6) >= 650541 && ccDigits.At(6) <= 650598) ||
+		(ccDigits.At(6) >= 650700 && ccDigits.At(6) <= 650718) ||
+		(ccDigits.At(6) >= 650720 && ccDigits.At(6) <= 650727) ||
+		(ccDigits.At(6) >= 650901 && ccDigits.At(6) <= 650920) ||
+		(ccDigits.At(6) >= 651652 && ccDigits.At(6) <= 651679) ||
+		(ccDigits.At(6) >= 655000 && ccDigits.At(6) <= 655019) ||
+		(ccDigits.At(6) >= 655021 && ccDigits.At(6) <= 655021):
+		return Company{"elo", "Elo"}, nil
+
+	case ccDigits.At(6) >= 604201 && ccDigits.At(6) <= 604219:
+		return Company{"SC", "Standard Chartered"}, nil
+
+	case ccDigits.At(6) == 384100 || ccDigits.At(6) == 384140 || ccDigits.At(6) == 384160 ||
+		ccDigits.At(6) == 606282 || ccDigits.At(6) == 637095 || ccDigits.At(4) == 637568 ||
+		ccDigits.At(4) == 637599 || ccDigits.At(4) == 637609 || ccDigits.At(4) == 637612:
+		return Company{"acb", "ACB"}, nil
+
+	case ccDigits.At(2) == 34 || ccDigits.At(2) == 37:
+		return Company{"vcb", "VietcomBank"}, nil
+
+	case ccDigits.At(4) == 5610 || (ccDigits.At(6) >= 560221 && ccDigits.At(6) <= 560225):
+		return Company{"abc", "ABC Card"}, nil
+
+	case ccDigits.At(2) == 62:
+		return Company{"tpb", "TienPhong Bank"}, nil
+
+	case ccDigits.At(3) >= 300 && ccDigits.At(3) <= 305 && ccLen == 15:
+		return Company{"ocb", "OCB"}, nil
+
+	case ccDigits.At(2) >= 51 && ccDigits.At(2) <= 55:
+		return Company{"mastercard", "Mastercard"}, nil
+
+	case ccDigits.At(2) == 35:
+		return Company{"jcb", "JCB"}, nil
+
+	case ccDigits.At(1) == 4:
+		return Company{"visa", "Visa"}, nil
+
+	default:
+		return Company{"", ""}, errors.New("Unknow Credit Card Brand")
+	}
+
 }
 
 // Use Luhn algorithm check the credit card's number against the Luhn algorithm
 func (c *Card) ValidateNumber() bool {
-	// implemented here
-	return true
+	var sum, mod int
+	var alternate bool
+	var err error
+
+	// get card number length
+	numberLen := len(c.Number)
+
+	if numberLen < 13 || numberLen > 16 {
+		return false
+	}
+	// Parse all numbers of the card into a for loop
+	for i := numberLen - 1; i > -1; i-- {
+		// Takes the mod, converting the current number in integer
+		if mod, err = strconv.Atoi(string(c.Number[i])); err != nil {
+			return false
+		}
+		if alternate {
+			mod *= 2
+			if mod > 9 {
+				mod = (mod % 10) + 1
+			}
+		}
+		alternate = !alternate
+		sum += mod
+	}
+	return sum%10 == 0
 }
