6c745b852c9a36d50a0d3645c34ed7c5bb4875d5
SilverOmega <nguyenducanh8993@gmail.com>
2023-08-18 16:18:46 +0700

    update BenchmarkCard_LastFour

--- creditcard_test.go
+++ creditcard_test.go
@@ -1,12 +1,26 @@
 package cards_validation
 
 import (
+	"bytes"
 	_ "strconv"
 	"testing"
 	_ "testing"
 	_ "time"
 )
 
+func LastFour(msg []byte) {
+	buf := new(bytes.Buffer)
+	buf.Write(msg)
+}
+
+func BenchmarkCard_LastFour(b *testing.B) {
+	msg := []byte("number is not long enough")
+	for i := 0; i < b.N; i++ { // internal bench stuff handle
+		for i := 0; i < 4; i++ {
+			LastFour([]byte(msg))
+		}
+	}
+}
 func BenchmarkSlienceNonAllocate(b *testing.B) {
 	n := 100
 	b.Run("test Benchmark Slice Non Allocate", func(b *testing.B) {
