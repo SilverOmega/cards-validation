fb96e0175b86c5d8744e1892eb04b15b3c8999fd
SilverOmega <nguyenducanh8993@gmail.com>
2023-09-19 15:20:54 +0700

    update HTTTP Client

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package cards_validation
 
 import (
+	"encoding/json"
 	"fmt"
 	_ "fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	_ "net/http"
 )
 
 func main() {
+	///add feature for link just use http.HandleFunc:
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/api/facts", CatFacts)
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
@@ -19,4 +23,28 @@ func main() {
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func CatFacts(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=29")
+	if err != nil {
+		log.Printf("Error getting data %s", err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error parsing data %s", err.Error())
+	}
+
+	var catFacts []CatFact
+	err = json.Unmarshal(body, &catFacts)
+	if err != nil {
+		log.Printf("Error parsing json %s", err.Error())
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(catFacts)
+	if err != nil {
+		log.Printf("Error parsing json %s", err.Error())
+	}
 }
